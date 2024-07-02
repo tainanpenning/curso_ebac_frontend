a0f7edf36bb5944483505d57da27c84b09a29aa9
tainanpenning <tainanhartwigpenning@gmail.com>
2024-07-02 17:44:39 -0300

    exercicio_jquery_corrigido

--- main.js
+++ main.js
@@ -6,10 +6,6 @@ $(document).ready(function() {
     $('#botao-cancelar').click(function() {
         $('.container').slideUp(300)
     })
-    
-    $('li').click(function(){
-        $('li').toggleClass('line-through');
-    })
 
     $('form').on('submit', function(e) {
         e.preventDefault()
@@ -22,12 +18,16 @@ $(document).ready(function() {
         $(`<h3 class="nome-tarefa" style="font-weight: 400;">${nomeTarefa}</h3>`).appendTo(novaTarefa)
         $(`<h3 class="data-tarefa" style="font-weight: 400;">${data}</h3>`).appendTo(novaTarefa)
         $(`<h3 class="hora-tarefa" style="font-weight: 400;">${hora}</h3>`).appendTo(novaTarefa)
-        $(novaTarefa).appendTo('ol')
+        $(novaTarefa).appendTo('ul')
         $(novaTarefa).fadeIn(1000)
 
         $('#input-nome').val('')
         $('#input-data').val('')
         $('#input-hora').val('')
+    })
+
+    $('ul').on('click', 'li', function(){
+        $(this).toggleClass('line-through');
     })
 })
 
